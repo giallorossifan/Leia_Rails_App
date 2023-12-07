611e926a4c19dad5168c663eafdfd88d8ae4e272
Ubuntu <ubuntu@ip-172-31-22-63.us-east-2.compute.internal>
2023-12-07 19:15:35 +0000

    user login test are now greeen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,17 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:email])
+      # Log the user in and redirectr to the users's show page.
+    else
+      flash.now[:danger] = 'Invalid email/password combination' #not quite right!
+    render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
 end
