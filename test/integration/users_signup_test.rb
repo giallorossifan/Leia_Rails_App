a39b5250e53041fcd8fb50532f22c21c0c829a24
Ubuntu <ubuntu@ip-172-31-22-63.us-east-2.compute.internal>
2023-12-07 18:38:59 +0000

    added login page but navigation link not yet working

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,9 +15,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
+    
     assert_difference 'User.count', 1 do
       post user_path, params: { user:  { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
     end
+    
     follow_redirect!
     assert_template 'users/show'
   end
