42c477a2b16a93fb0db0b40506d2c525906b0cdb
Ubuntu <ubuntu@ip-172-31-22-63.us-east-2.compute.internal>
2023-12-14 04:58:45 +0000

    created friendly forwarding test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,10 @@ module SessionsHelper
 
   def log_in(user)
     session[:user_id] = user.id
+
+    # Guard againts session replay attacks.
+    # See https://bit.ly/33UvKOw for more.
+    session[:session_token] = user.session_token
   end
 
   # Remember a user in a persitent session
@@ -16,7 +20,10 @@ module SessionsHelper
   # return the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -24,6 +31,12 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+
+  # Return sture is the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise
@@ -45,6 +58,10 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # Stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 
 
 
