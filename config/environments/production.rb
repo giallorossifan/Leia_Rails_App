3da39d7c2ca764bd0151292783cd8483b69142d7
Ubuntu <ubuntu@ip-172-31-22-63.us-east-2.compute.internal>
2024-01-05 21:56:22 +0000

    changed :plain to :login in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
-    host: 'leia-bear-app.herokuapp.com',
+    host: 'leia-bear-app-26e4fa9c5b9f.herokuapp.com',
     protocol: 'https'
   }
 
   config.action_mailer.smtp_settings = {
     address: Rails.application.credentials.dig(:aws, :ses_address),
     port: 587,
-    authentication: :plain,
+    authentication: :login,
     user_name: Rails.application.credentials.dig(:aws, :ses_username),\
     password: Rails.application.credentials.dig(:aws, :ses_password),
     #authentication: :login,
