8f7cbe363d64fa3e0a141ae5b97d75bf19f45e02
Ubuntu <ubuntu@ip-172-31-22-63.us-east-2.compute.internal>
2024-01-16 23:35:25 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token,:activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -60,13 +60,32 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    #update_columns(reset_digest: true, reset_sent_at: Time.zone.now)
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Return true if a password reset has expired
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+
 
   private
 
   # converts email to all lowercase.
   def downcase_email
-   # self.email = email.downcase
-   email.downcase!
+   self.email = email.downcase
+   #self.email.downcase!
   end
 
   # Creeates and assigns the activation token and digest.
