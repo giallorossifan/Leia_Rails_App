38f7fda8f315e6f998743b0e1ded805e5b26d333
Ubuntu <ubuntu@ip-172-31-22-63.us-east-2.compute.internal>
2024-01-23 19:32:02 +0000

    generated micropost model

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@ class ApplicationMailer < ActionMailer::Base
 
   #default from: "user@realdomain.com"
   #default from: " Leia <hello@leiabearandfriends.com>"
-  default from: "user@leiabearandfriends.com"
+  default from: "no-reply@leiabearandfriends.com"
   layout "mailer"
 end
